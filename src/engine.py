320dc34cac2210cda0c36826a59ad7a1b2192b0b
aayushyatiwari <kb24.aayush@gmail.com>
2026-08-29 10:07:39 +0530

    updated config

--- src/engine.py
+++ src/engine.py
@@ -128,7 +128,7 @@ class EarlyStopping:
     """
     A simple early stopping mechanism to prevent overfitting.
     """
-    def __init__(self, patience=5, min_delta=1e-5):
+    def __init__(self, patience=5, min_delta=1e-4):
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
